07c2246177ecd243a22660f0da02a203f3c6413b
Jaderson <jaderson.brandao@totvs.com.br>
2018-03-19 17:12:59 -0300

    6.5. Configurando JWT no projeto

--- src/main/java/com/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -17,7 +17,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
 import com.algamoney.api.model.Lancamento;
-import com.algamoney.api.model.Lancamento_;
 import com.algamoney.api.repository.filter.LancamentoFilter;
 
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
@@ -48,16 +47,16 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 		List<Predicate> predicates = new ArrayList<>();
 		
 		if(!StringUtils.isEmpty( lancamentoFilter.getDescricao())){
-			predicates.add(builder.like(builder.lower(root.get(Lancamento_.descricao)),
+			predicates.add(builder.like(builder.lower(root.get("descricao")),
 					"%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
 		}
 		if(lancamentoFilter.getDataVencimentoDe() != null){
 			predicates.add(
-					builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoDe()));
+					builder.greaterThanOrEqualTo(root.get("dataVencimento"), lancamentoFilter.getDataVencimentoDe()));
 		}
 		if(lancamentoFilter.getDataVencimentoAte() != null){
 			predicates.add(
-					builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento),lancamentoFilter.getDataVencimentoAte()));
+					builder.lessThanOrEqualTo(root.get("dataVencimento"),lancamentoFilter.getDataVencimentoAte()));
 		}
 		
 		return predicates.toArray(new Predicate[predicates.size()]);
