488f3aa7d62787840bdeeda55a88d8d82735df5a
Jaderson <jaderson.brandao@totvs.com.br>
2018-03-28 18:32:27 -0300

    7.2. Profiles do Spring

--- src/main/java/com/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/algamoney/api/repository/lancamento/LancamentoRepositoryImpl.java
@@ -18,6 +18,7 @@ import org.springframework.util.StringUtils;
 
 import com.algamoney.api.model.Lancamento;
 import com.algamoney.api.repository.filter.LancamentoFilter;
+import com.algamoney.api.repository.projection.ResumoLancamento;
 
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 	
@@ -39,6 +40,28 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 		
 		
 		return new PageImpl<>( query.getResultList(), pageable, total(lancamentoFilter));
+	}
+	@Override
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<ResumoLancamento> criteria = builder.createQuery(ResumoLancamento.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class);
+		
+		criteria.select(builder.construct(ResumoLancamento.class
+				, root.get("codigo"), root.get("descricao")
+				, root.get("dataVencimento"), root.get("dataPagamento")
+				, root.get("valor"), root.get("tipo")
+				, root.get("categoria"), root.get("nome")
+				, root.get("pessoa"), root.get("nome")));
+		
+		
+		Predicate[] predicates = criaRestricoes(lancamentoFilter, builder, root);
+		criteria.where(predicates);
+		
+		TypedQuery<ResumoLancamento> query = manager.createQuery(criteria);
+		adicionarRestricoesDePaginacao(query, pageable);
+		
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
 	}
 
 	private Predicate[] criaRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
@@ -62,7 +85,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 		return predicates.toArray(new Predicate[predicates.size()]);
 	}
 	
-	private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {		
+	private void adicionarRestricoesDePaginacao(TypedQuery<?> query, Pageable pageable) {		
 		int paginaAtual = pageable.getPageNumber();
 		int totalRegistrosPorPagina = pageable.getPageSize();
 		int primeiroRegistroDaPagina = paginaAtual + totalRegistrosPorPagina;
@@ -84,6 +107,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery{
 		
 		return manager.createQuery(criteria).getSingleResult();
 	}
+
 	
 
 }
--- src/main/java/com/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/algamoney/api/resource/LancamentoResource.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +32,7 @@ import com.algamoney.api.exceptionhandler.AlgamoneyExcpetionHandler.Erro;
 import com.algamoney.api.model.Lancamento;
 import com.algamoney.api.repository.LancamentoRepository;
 import com.algamoney.api.repository.filter.LancamentoFilter;
+import com.algamoney.api.repository.projection.ResumoLancamento;
 import com.algamoney.api.service.LancamentoService;
 import com.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
@@ -51,11 +53,19 @@ public class LancamentoResource {
 	private MessageSource messageSource;
 	
 	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable){
 		return this.lancamentoRepository.filtrar(lancamentoFilter, pageable);
-	}	
+	}
+	
+	@GetMapping(params = "resumo")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable){
+		return this.lancamentoRepository.resumir(lancamentoFilter, pageable);
+	}
 	
 	@GetMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public ResponseEntity<Optional<Lancamento>> buscaPeloCodigo(@PathVariable Long codigo, HttpServletResponse response) {
 		Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);		
 		if(lancamento.isPresent()){
@@ -65,6 +75,7 @@ public class LancamentoResource {
 	}
 	
 	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
 	public ResponseEntity<Lancamento> criar (@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
 		Lancamento lancamentoSalvo = this.lancamentoService.salvar(lancamento);
 		this.publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
@@ -72,11 +83,13 @@ public class LancamentoResource {
 	}
 	
 	@DeleteMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO') and #oauth2.hasScope('write')")
 	public void remover(@PathVariable Long codigo) {
 		this.lancamentoRepository.deleteById(codigo);
 	}
 	
 	@PutMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
 	public ResponseEntity<Lancamento> atualizar(@PathVariable Long codigo,@Valid @RequestBody Lancamento lancamento) {
 		Lancamento lancamentoSalvo = this.lancamentoService.atualiza(codigo, lancamento);
 		return ResponseEntity.ok(lancamentoSalvo);
